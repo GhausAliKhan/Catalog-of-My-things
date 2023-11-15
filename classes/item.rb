2aa802a6cbbaf43e02fe491eacf624ce5a7d966a
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-15 20:56:55 +0500

    Define properties in the constructor

--- classes/item.rb
+++ classes/item.rb
@@ -1,4 +1,10 @@
 require 'date'
 
 class Item
+  attr_accessor :archived, :source, :publish_date, :id, :genre, :can_be_archived, :label
+
+  def initialize(publish_date, id = nil)
+    @id = id || Random.rand(1..1000)
+    @publish_date = publish_date
+  end
 end
