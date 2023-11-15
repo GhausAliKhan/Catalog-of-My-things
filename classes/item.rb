9b020ed2aace961a919ec69738b7ede479c984cf
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-11-15 16:54:54 +0000

    added method move_to_achieve

--- classes/item.rb
+++ classes/item.rb
@@ -32,4 +32,8 @@ class Item
     publish_year = Date.parse(@publish_date).year
     current_year - publish_year > 10
   end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 end
