2e3a30b16018e25eb9bf3c634bc26aaa14ee5c94
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-16 21:05:09 +0500

    Include/Require all methods in modules.rb File

--- modules/modules.rb
+++ modules/modules.rb
@@ -1,3 +1,8 @@
+require_relative 'movie/addmovie'
+require_relative 'movie/displaymovie'
+require_relative 'movie/displaysources'
+require_relative 'movie/preservemoviesource'
+require_relative 'movie/load_movie_source'
 require_relative 'book/addbook'
 require_relative 'book/displaybook'
 require_relative 'book/displaylabel'
@@ -7,8 +12,18 @@ require_relative 'music/addmusic'
 require_relative 'music/displaymusic'
 require_relative 'music/displaygenre'
 require_relative 'music/storage'
+require_relative 'game/addgame'
+require_relative 'game/displaygame'
+require_relative 'game/displayauthor'
+require_relative 'game/preservegameauthor'
+require_relative 'game/load_game_author'
 
 module Modules
+  include AddMovie
+  include DisplayMovie
+  include DisplaySources
+  include PreserveMovieSource
+  include LoadMovieSource
   include AddBook
   include DisplayBook
   include DisplayLabel
@@ -18,17 +33,30 @@ module Modules
   include DisplayMusic
   include DisplayGenres
   include Storage
+  include AddGame
+  include DisplayGame
+  include DisplayAuthor
+  include PreserveGameAuthor
+  include LoadGameAuthor
 
   def save_collections
+    save_movies
+    save_source
     save_book
     save_label
     save_music
+    save_games
+    save_author
   end
 
   def load_collections
+    load_sources
+    load_movies
     load_labels
     load_books
     load_genres
     load_music_albums
+    load_authors
+    load_games
   end
 end
