1cf3158efee17c2b026d44fe94414505b16b6b8d
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-16 21:06:50 +0500

    Add table source and movie to schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE catalogue_of_my_things;
+
 CREATE TABLE books (
   id INT GENERATED ALWAYS AS IDENTITY,
   publish_date DATE,
@@ -12,11 +14,24 @@ CREATE TABLE labels (
  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
  color VARCHAR(255),
  title VARCHAR(255),
- items TEXT
- []
+ items TEXT[]
 );
 
- CREATE TABLE games (
+CREATE TABLE genre (
+  id INT PRIMARY KEY NOT NULL,
+  name VARCHAR(100),
+  items TEXT[]
+);
+    
+CREATE TABLE music_album (
+  id INT PRIMARY KEY NOT NULL,
+  on_spotify BOOLEAN,
+  publish_date DATE,
+  archived BOOLEAN,
+  genre_id INT REFERENCES genre(id)
+);
+
+CREATE TABLE games (
   id INT GENERATED ALWAYS AS IDENTITY,
   publish_date DATE,
   author_id INT REFERENCES authors(id),
@@ -30,23 +45,20 @@ CREATE TABLE Authors (
  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
  first_name VARCHAR(255),
  last_name VARCHAR(255),
- items TEXT
-[]
+ items TEXT[]
 );
 
-CREATE TABLE genre
-(
-    id INT PRIMARY KEY NOT NULL,
-    name VARCHAR(100),
-    items TEXT
-    []
+CREATE TABLE source (
+  id INT PRIMARY KEY NOT NULL,
+  name VARCHAR(100),
+  items TEXT
+  []
 );
-    
-CREATE TABLE music_album
-(
-    id INT PRIMARY KEY NOT NULL,
-    on_spotify BOOLEAN,
-    publish_date DATE,
-    archived BOOLEAN,
-    genre_id INT REFERENCES genre(id)
+
+CREATE TABLE movie (
+  id INT PRIMARY KEY NOT NULL,
+  silent BOOLEAN,
+  publish_date DATE,
+  archived BOOLEAN,
+  source_id INT REFERENCES source(id)
 );
